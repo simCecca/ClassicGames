68e5ebf3f081a52b4d32d5a3432841b34be634cb
Simone Ceccarelli <ceccarellisimone1@gmail.com>
2019-03-02 11:22:35 +0100

    Updates

---
+++ precache-manifest.78272743c42df0d17ebd5184694832d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClassicGames/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0545c6ca19e584ed20b6",
+    "url": "/ClassicGames/static/js/runtime~main.0545c6ca.js"
+  },
+  {
+    "revision": "11f827d33dea819aeb97",
+    "url": "/ClassicGames/static/js/main.11f827d3.chunk.js"
+  },
+  {
+    "revision": "ce16120cb26415fe3d86",
+    "url": "/ClassicGames/static/js/2.ce16120c.chunk.js"
+  },
+  {
+    "revision": "11f827d33dea819aeb97",
+    "url": "/ClassicGames/static/css/main.6b342c7a.chunk.css"
+  },
+  {
+    "revision": "ce16120cb26415fe3d86",
+    "url": "/ClassicGames/static/css/2.8ab53b01.chunk.css"
+  },
+  {
+    "revision": "ed3652a0a5d21bdadb54d8b29d8463e0",
+    "url": "/ClassicGames/index.html"
+  }
+];
